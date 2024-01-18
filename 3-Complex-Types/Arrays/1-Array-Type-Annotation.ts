638a3a415a1d3b96b62d5e3d41a4641689ad6b7e
Adam Turner <hello@adamrichardturner.dev>
2024-01-18 06:27:25 +0000

    Add folder structure and array type notes

---
+++ 3-Complex-Types/Arrays/1-Array-Type-Annotation.ts
@@ -0,0 +1,10 @@
+// Arrays:
+let bestNumbers: number[] = [7,77,4];
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfasts: string[]= ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleans: boolean[] = [true, false];
+
+// Multidimensional Arrays:
+let bestMealPlan: string[][] = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
+let bestBooleansTwice: boolean[][] = [bestBooleans, bestBooleans];
+let numbersMulti: number[][][] = [ [[1],[2,3]], [[7],bestNumbers] ];
